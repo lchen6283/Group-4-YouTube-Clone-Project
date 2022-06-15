6973668dc5514ddc8dea553d53f819e46963d248
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-06-15 12:22:07 -0400

    About Page...
    Still working on About Page

--- src/components/About.js
+++ src/components/About.js
@@ -3,20 +3,18 @@ import "./About.css";
 const AboutMeSection = () => {
   return (
     <>
-      <body>
+      
         <h4>Engineered By:</h4>
         {/* <h6>Emalee Soto,<br></br> Candace Garvin, <br></br>and Lawrence Chan</h6> */}
         <div id="card">
           <h6>Emalee Soto</h6>
           <img />
-        </div>
         <h6>Candace Garvin</h6>
         <img />
-        <div id="card"></div>
         <h6> Lawrence Chan</h6>
         <img />
-        <div id="card"></div>
-        <h4>About Us:</h4>
+         <h4>About Us:</h4>
+        </div>
         <p>
           We are currently Jr. Software Engineers at Pursuit located in Queens,
           NY. The purpose of this app is create our rendition of the world
@@ -25,7 +23,7 @@ const AboutMeSection = () => {
           video by title or content and be met with thousands of video that meet
           their search. We hope we have done it justice. Thanks for stopping by.{" "}
         </p>
-      </body>
+      
     </>
   );
 };
