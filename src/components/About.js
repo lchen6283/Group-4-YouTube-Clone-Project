f243e3af07ed02f9708528abc6bc7eaef8aac224
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-06-15 00:58:12 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const AboutMeSection = () => {
 return (
   <>
  
-  <p>
+  <body>
   
     <h4>Engineered By:</h4>
     <h6>Emalee Soto,<br></br> Candace Garvin, <br></br>and Lawrence Chan</h6>
@@ -20,7 +20,7 @@ return (
     <div id="card"></div> */}
     <h4>About Us:</h4>
     <p>We are currently Jr. Software Engineers at Pursuit located in Queens, NY. The purpose of this app is create our rendition of the world famous YouTube application. Together we have compiled our tech as well as creative skills to create an app where a user can search for a video by title or content and be met with thousands of video that meet their search. We hope we have done it justice. Thanks for stopping by. </p>
-    </p>
+    </body>
    
   </>
 );
