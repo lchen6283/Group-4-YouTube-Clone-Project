718d711b1a053ab228877ea8f88422419998aa31
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-06-16 10:13:59 -0400

    Update Video.js

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,7 +1,7 @@
 const Video = ({ video }) => {
   const { snippet, id } = video;
   const { thumbnails } = snippet;
-  return <img src={thumbnails.high.url} alt={id.videoid} />;
+  return <img src={thumbnails.high.url} alt={id.videoid} />
 };
 
 export default Video;
